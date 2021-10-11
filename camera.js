1c728ab648eef2bf2a0744049dd9f0cc36e69fac
Dincă Silviu Cristian <silviudinca412@gmail.com>
2021-10-11 10:41:41 +0300

    Add files via upload

---
+++ camera.js
@@ -0,0 +1,24 @@
+let video = document.getElementById("video");
+
+function on_cam_succes(stream){
+    video.srcObject = stream;
+}
+
+function on_cam_error(err){
+    alert("error " + err);
+}
+
+let constrains = {
+    audio : false,
+    video : true
+};
+
+navigator.mediaDevices.getUserMedia(constrains).then(on_cam_succes).catch(on_cam_error);
+
+function capteaza() {
+    let c = document.getElementById("canvas");
+    c.width = video.width;
+    c.height = video.height;
+    let ctx = c.getContext("2d");
+    ctx.drawImage(video,0,0,640,480);
+}
