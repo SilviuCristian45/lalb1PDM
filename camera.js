35b71586202df5c8e6b5bc5941841b3969613e92
Dincă Silviu Cristian <silviudinca412@gmail.com>
2021-10-11 11:16:31 +0300

    Add files via upload

--- camera.js
+++ camera.js
@@ -16,9 +16,13 @@ let constrains = {
 navigator.mediaDevices.getUserMedia(constrains).then(on_cam_succes).catch(on_cam_error);
 
 function capteaza() {
+    console.log("captam");
     let c = document.getElementById("canvas");
     c.width = video.width;
     c.height = video.height;
     let ctx = c.getContext("2d");
     ctx.drawImage(video,0,0,640,480);
 }
+
+video.addEventListener("touchstart",capteaza);
+video.addEventListener("mousedown",capteaza);
